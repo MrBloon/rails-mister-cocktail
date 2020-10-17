50cb68577c9fc350caaf1d64bae490e3e9808172
matthieu hedin <hedin.matthieu75@gmail.com>
2020-10-17 17:53:08 +0200

    back

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Black Russian")
+Cocktail.create(name: "Appletini")
 
 require 'json'
 require 'open-uri'
